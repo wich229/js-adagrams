802d217a3895b211e562680c6696286a2fd4558e
Charles Ellis <hamled@hamled.name>
2018-11-08 19:12:05 -0800

    Tests for Adagrams.scoreWord()

--- specs/adagrams.spec.js
+++ specs/adagrams.spec.js
@@ -44,4 +44,43 @@ describe('Adagrams', () => {
 
     });
   });
+
+  describe('scoreWord', () => {
+    const expectScores = (wordScores) => {
+      Object.entries(wordScores).forEach(([word, score]) => {
+        expect(Adagrams.scoreWord(word)).toBe(score);
+      });
+    };
+
+    it('returns an accurate numerical score according to the score chart', () => {
+      expectScores({
+        'A': 1,
+        'DOG': 5,
+        'WHIMSY': 17
+      });
+    });
+
+    it('returns a score regardless of the input case', () => {
+      expectScores({
+        'a': 1,
+        'dog': 5,
+        'wHiMsY': 17
+      });
+    });
+
+    it('returns a score of 0 if given an empty input', () => {
+      expectScores({
+        '': 0
+      });
+    });
+
+    it('adds an extra 8 points if word is 7 or more characters long', () => {
+      expectScores({
+        'XXXXXXX': 64,
+        'XXXXXXXX': 72,
+        'XXXXXXXXX': 80,
+        'XXXXXXXXXX': 88
+      });
+    });
+  });
 });
