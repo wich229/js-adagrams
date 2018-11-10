0e91ea2db0f2d8b4092c0c3ead855259ae76cd38
Charles Ellis <hamled@hamled.name>
2018-11-10 13:10:59 -0800

    Adagrams stub
    This is a stub implementation of the Adagrams library. Each method will
    automatically fall through to the student's real implementation if it
    exists.

---
+++ src/game/adagrams.js
@@ -0,0 +1,44 @@
+import Real from 'adagrams';
+
+const Stub = {
+  drawLetters() {
+    const defaultLetters = ['H', 'E', 'L', 'L', 'O', 'W', 'O', 'R', 'L', 'D'];
+
+    if(typeof Real.drawLetters === 'function') {
+      return Real.drawLetters() || defaultLetters;
+    }
+
+    return defaultLetters;
+  },
+
+  usesAvailableLetters(input, lettersInHand) {
+    if(typeof Real.usesAvailableLetters === 'function') {
+      return Real.usesAvailableLetters(input, lettersInHand);
+    }
+
+    return true;
+  },
+
+  scoreWord(word) {
+    if(typeof Real.scoreWord === 'function') {
+      return Real.scoreWord(word);
+    }
+
+    return -1;
+  },
+
+  highestScoreFrom(words) {
+    if(typeof Real.highestScoreFrom === 'function') {
+      return Real.highestScoreFrom(words);
+    }
+
+    if(words.length < 1) return null;
+
+    return {
+      word: words[0],
+      score: Stub.scoreWord(words[0]),
+    };
+  },
+};
+
+export default Stub;
