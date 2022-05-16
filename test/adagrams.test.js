1775c3a936d79a6154de9b39506166e1b8104e3f
Becca Elenzil <becca@adadevelopersacademy.org>
2022-05-16 09:16:07 -0700

    add test does not draw a letter too many times

--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -1,14 +1,43 @@
-import Adagrams from 'adagrams';
+import Adagrams from "adagrams";
 
-describe('Adagrams', () => {
-  describe('drawLetters', () => {
-    it('draws ten letters from the letter pool', () => {
+LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
+describe("Adagrams", () => {
+  describe("drawLetters", () => {
+    it("draws ten letters from the letter pool", () => {
       const drawn = Adagrams.drawLetters();
 
       expect(drawn).toHaveLength(10);
     });
 
-    it('returns an array, and each item is a single-letter string', () => {
+    it("returns an array, and each item is a single-letter string", () => {
       const drawn = Adagrams.drawLetters();
 
       expect(Array.isArray(drawn)).toBe(true);
@@ -16,65 +45,82 @@ describe('Adagrams', () => {
         expect(l).toMatch(/^[A-Z]$/);
       });
     });
+
+    it("does not draw a letter too many times", () => {
+      for (let i = 0; i < 1000; i++) {
+        const drawn = Adagrams.drawLetters();
+        letter_freq = {};
+        for (letter of letters) {
+        }
+        if (letter in letter_freq) {
+          letter_freq[letter] += 1;
+        } else {
+          letter_freq[letter] = 1;
+        }
+
+        for (letter of letters) {
+          expect(letter_freq[letter]).toBeLessThanOrEqual(LETTER_POOL[letter]);
+        }
+      }
+    });
   });
 
-  describe('usesAvailableLetters', () => {
-    it('returns true if the submitted letters are valid against the drawn letters', () => {
-      const drawn = ['D', 'O', 'G', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
-      const word = 'DOG';
+  describe("usesAvailableLetters", () => {
+    it("returns true if the submitted letters are valid against the drawn letters", () => {
+      const drawn = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"];
+      const word = "DOG";
 
       const isValid = Adagrams.usesAvailableLetters(word, drawn);
       expect(isValid).toBe(true);
     });
 
-    it('returns false when word contains letters not in the drawn letters', () => {
-      const drawn = ['D', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
-      const word = 'DOG';
+    it("returns false when word contains letters not in the drawn letters", () => {
+      const drawn = ["D", "O", "X", "X", "X", "X", "X", "X", "X", "X"];
+      const word = "DOG";
 
       const isValid = Adagrams.usesAvailableLetters(word, drawn);
       expect(isValid).toBe(false);
     });
 
-    it('returns false when word contains repeated letters more than in the drawn letters', () => {
-      const drawn = ['D', 'O', 'G', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
-      const word = 'GOOD';
+    it("returns false when word contains repeated letters more than in the drawn letters", () => {
+      const drawn = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"];
+      const word = "GOOD";
 
       const isValid = Adagrams.usesAvailableLetters(word, drawn);
       expect(isValid).toBe(false);
-
     });
   });
 
-  describe('scoreWord', () => {
+  describe("scoreWord", () => {
     const expectScores = (wordScores) => {
       Object.entries(wordScores).forEach(([word, score]) => {
         expect(Adagrams.scoreWord(word)).toBe(score);
       });
     };
 
-    it('returns an accurate numerical score according to the score chart', () => {
+    it("returns an accurate numerical score according to the score chart", () => {
       expectScores({
         A: 1,
         DOG: 5,
-        WHIMSY: 17
+        WHIMSY: 17,
       });
     });
 
-    it('returns a score regardless of the input case', () => {
+    it("returns a score regardless of the input case", () => {
       expectScores({
         a: 1,
         dog: 5,
-        wHiMsY: 17
+        wHiMsY: 17,
       });
     });
 
-    it('returns a score of 0 if given an empty input', () => {
+    it("returns a score of 0 if given an empty input", () => {
       expectScores({
-        '': 0
+        "": 0,
       });
     });
 
-    it('adds an extra 8 points if word is 7 or more characters long', () => {
+    it("adds an extra 8 points if word is 7 or more characters long", () => {
       expectScores({
         XXXXXXX: 64,
         XXXXXXXX: 72,
@@ -84,53 +130,62 @@ describe('Adagrams', () => {
     });
   });
 
-  describe.skip('highestScoreFrom', () => {
-    it('returns a hash that contains the word and score of best word in an array', () => {
-      const words = ['X', 'XX', 'XXX', 'XXXX'];
-      const correct = { word: 'XXXX', score: Adagrams.scoreWord('XXXX') };
+  describe.skip("highestScoreFrom", () => {
+    it("returns a hash that contains the word and score of best word in an array", () => {
+      const words = ["X", "XX", "XXX", "XXXX"];
+      const correct = { word: "XXXX", score: Adagrams.scoreWord("XXXX") };
 
       expect(Adagrams.highestScoreFrom(words)).toEqual(correct);
     });
 
-    it('accurately finds best scoring word even if not sorted', () => {
-      const words = ['XXX', 'XXXX', 'X', 'XX'];
-      const correct = { word: 'XXXX', score: Adagrams.scoreWord('XXXX') };
+    it("accurately finds best scoring word even if not sorted", () => {
+      const words = ["XXX", "XXXX", "X", "XX"];
+      const correct = { word: "XXXX", score: Adagrams.scoreWord("XXXX") };
 
       expect(Adagrams.highestScoreFrom(words)).toEqual(correct);
     });
 
-    describe('in case of tied score', () => {
+    describe("in case of tied score", () => {
       const expectTie = (words) => {
-        const scores = words.map(word => Adagrams.scoreWord(word));
-        const highScore = scores.reduce((h, s) => h < s ? s : h, 0);
+        const scores = words.map((word) => Adagrams.scoreWord(word));
+        const highScore = scores.reduce((h, s) => (h < s ? s : h), 0);
         const tiedWords = scores.filter((s) => s == highScore);
 
         // Expect at least two tied words
         expect(tiedWords.length).toBeGreaterThan(1);
       };
 
-      it('selects the word with 10 letters', () => {
-        const words = ['AAAAAAAAAA', 'BBBBBB'];
-        const correct = { word: 'AAAAAAAAAA', score: Adagrams.scoreWord('AAAAAAAAAA') };
+      it("selects the word with 10 letters", () => {
+        const words = ["AAAAAAAAAA", "BBBBBB"];
+        const correct = {
+          word: "AAAAAAAAAA",
+          score: Adagrams.scoreWord("AAAAAAAAAA"),
+        };
         expectTie(words);
 
         expect(Adagrams.highestScoreFrom(words)).toEqual(correct);
         expect(Adagrams.highestScoreFrom(words.reverse())).toEqual(correct);
       });
 
-      it('selects the word with fewer letters when neither are 10 letters', () => {
-        const words = ['MMMM', 'WWW'];
-        const correct = { word: 'WWW', score: Adagrams.scoreWord('WWW') };
+      it("selects the word with fewer letters when neither are 10 letters", () => {
+        const words = ["MMMM", "WWW"];
+        const correct = { word: "WWW", score: Adagrams.scoreWord("WWW") };
         expectTie(words);
 
         expect(Adagrams.highestScoreFrom(words)).toEqual(correct);
         expect(Adagrams.highestScoreFrom(words.reverse())).toEqual(correct);
       });
 
-      it('selects the first word when both have same length', () => {
-        const words = ['AAAAAAAAAA', 'EEEEEEEEEE'];
-        const first = { word: 'AAAAAAAAAA', score: Adagrams.scoreWord('AAAAAAAAAA') };
-        const second = { word: 'EEEEEEEEEE', score: Adagrams.scoreWord('EEEEEEEEEE') };
+      it("selects the first word when both have same length", () => {
+        const words = ["AAAAAAAAAA", "EEEEEEEEEE"];
+        const first = {
+          word: "AAAAAAAAAA",
+          score: Adagrams.scoreWord("AAAAAAAAAA"),
+        };
+        const second = {
+          word: "EEEEEEEEEE",
+          score: Adagrams.scoreWord("EEEEEEEEEE"),
+        };
         expectTie(words);
 
         expect(Adagrams.highestScoreFrom(words)).toEqual(first);
