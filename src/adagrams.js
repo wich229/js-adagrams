545b23a27dcbc1d075d8fcea817e8f3f8ceb28da
Eva Liu <una229@gmail.com>
2023-01-19 02:54:09 -0500

    usesAvailableLetters and scoreWord functions completed

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,11 +27,32 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+// const LETTER_SCORE = {
+//   1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+//   2: ["D", "G"],
+//   3: ["B", "C", "M", "P"],
+//   4: ["F", "H", "V", "W", "Y"],
+//   5: "K",
+//   8: ["J", "X"],
+//   10: ["Q", "Z"],
+// };
+
+const LETTER_SCORE = {
+  AEIOUNRST: 1,
+  DG: 2,
+  BCMP: 3,
+  FHVWY: 4,
+  K: 5,
+  JX: 8,
+  QZ: 10,
+};
+
 export const drawLetters = () => {
   // const copy_pool = structuredClone(LETTER_POOL); #not able to use deep copy
   const copy_pool = { ...LETTER_POOL };
   let random_ten_letters = [];
   const keys_arr = Object.keys(copy_pool);
+
   let random_key;
   while (random_ten_letters.length < 10) {
     random_key = keys_arr[Math.floor(Math.random() * keys_arr.length)];
@@ -46,11 +67,41 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  const input_arr = input.split("");
+  if (input_arr.length > lettersInHand.length) {
+    return false;
+  }
+
+  for (let i = 0; i < input_arr.length; ++i) {
+    if (lettersInHand.includes(input_arr[i]) === false) {
+      return false;
+    }
+    let index = lettersInHand.indexOf(input_arr[i]);
+    lettersInHand.splice(index, index + 1);
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  // if word is empty str return 0
+  if (word === "") return 0;
+
+  word = word.toUpperCase();
+  let total = 0;
+  let keys_arr = Object.keys(LETTER_SCORE);
+
+  for (let i = 0; i < word.length; ++i) {
+    for (let j = 0; j < keys_arr.length; ++j) {
+      if (keys_arr[j].indexOf(word[i]) !== -1) {
+        total += LETTER_SCORE[keys_arr[j]];
+      }
+    }
+  }
+
+  if (word.length >= 7) {
+    total += 8;
+  }
+  return total;
 };
 
 export const highestScoreFrom = (words) => {
--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -37,14 +37,18 @@ const LETTER_POOL = {
 describe("Adagrams", () => {
   describe("drawLetters", () => {
     it("draws ten letters from the letter pool", () => {
+      //Act
       const drawn = drawLetters();
 
+      //Assert
       expect(drawn).toHaveLength(10);
     });
 
     it("returns an array, and each item is a single-letter string", () => {
+      //Act
       const drawn = drawLetters();
 
+      //Assert
       expect(Array.isArray(drawn)).toBe(true);
       drawn.forEach((l) => {
         expect(l).toMatch(/^[A-Z]$/);
@@ -52,6 +56,7 @@ describe("Adagrams", () => {
     });
 
     it("does not draw a letter too many times", () => {
+      //Act
       for (let i = 0; i < 1000; i++) {
         const drawn = drawLetters();
         const letter_freq = {};
@@ -63,6 +68,7 @@ describe("Adagrams", () => {
           }
         }
 
+        //Assert
         for (let letter of drawn) {
           expect(letter_freq[letter]).toBeLessThanOrEqual(LETTER_POOL[letter]);
         }
@@ -120,7 +126,9 @@ describe("Adagrams", () => {
     });
 
     it("returns a score of 0 if given an empty input", () => {
-      throw "Complete test";
+      expectScores({
+        "": 0,
+      });
     });
 
     it("adds an extra 8 points if word is 7 or more characters long", () => {
